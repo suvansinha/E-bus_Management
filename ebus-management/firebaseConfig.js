3fdf7c7aa1764a8ba9437815d13df7ac65387b41
Suvan Sinha <71833649+suvansinha@users.noreply.github.com>
2025-09-13 19:25:02 +0530

    Update firebaseConfig.js

--- ebus-management/firebaseConfig.js
+++ ebus-management/firebaseConfig.js
@@ -10,8 +10,8 @@ const firebaseConfig = {
   projectId: "e-bus-management-dac6c",
   storageBucket: "e-bus-management-dac6c.firebasestorage.app",
   messagingSenderId: "1053043707359",
-  appId: "1:1053043707359:web:d3bd7f2eef51d23812886a",
-  measurementId: "G-H3M96SJ4TE"
+  appId: "1:1053043707359:web:973fe5d245422a3a12886a",
+  measurementId: "G-HXBR4FSRLL"
 };
 
 
